c411f524d0c64a9aa3288dcc91f97918d323217c
dchu <dchu@etcc.com>
2020-07-18 12:27:53 -0500

    initial commit: code skeleton and gradle setup.

---
+++ product-service/src/test/java/com/duc/microservices/core/product/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duc.microservices.core.product.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ review-service/src/main/java/com/duc/microservices/core/product/review/ReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.duc.microservices.core.product.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewServiceApplication.class, args);
+    }
+
+}
